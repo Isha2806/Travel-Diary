9830324f50cba8c694888b67a103e8dd487f533f
Isha <xishaapatel@gmail.com>
2022-07-14 13:56:42 +0530

    Add : ScribblePad

---
+++ src/Components/ScribblePad/scribble.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./scribble.css";
+export default function ScribblePad(){
+    return (<>
+    This is scribble pad!
+    </>);
+}
