7e075cde416b2df04028147dde224a7cbbb99595
Isha <xishaapatel@gmail.com>
2022-07-22 17:42:12 +0530

    created empty page

--- src/Components/EmptyPage/emptyPage.js
+++ src/Components/EmptyPage/emptyPage.js
@@ -1,4 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 export default function Empty(){
-    return(<>This is empty</>)
+    return(<>
+        <div className="emptyState" >
+            <h4>NO DIARIES HERE!</h4>
+            <Link to = "/">
+            <button>Go BACK</button>
+            </Link>
+        </div>
+    </>)
 }
